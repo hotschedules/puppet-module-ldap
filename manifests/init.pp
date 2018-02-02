45f0c666f021852c01d9169325f692ec58708877
Alex Yamauchi <alex.yamauchi@hotschedules.com>
2018-02-02 21:39:34 +0000

    INFRA-6410: adding non-NULL validation for required values

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,8 +23,17 @@ class ldap(
 
   validate_array    ( $packages       )
   validate_string   ( $security       )
+  if $cert_ca == undef {
+    fail("Parameter 'cert_ca' is required.")
+  }
   validate_string   ( $cert_ca        )
+  if $bind_dn == undef {
+    fail("Parameter 'bind_dn' is required.")
+  }
   validate_string   ( $bind_dn        )
+  if $bind_pass == undef {
+    fail("Parameter 'bind_pass' is required.")
+  }
   validate_string   ( $bind_pass      )
   validate_integer  ( $idle_timelimit )
 
